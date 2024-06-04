f0929f774b9e481333512c1d2514db023963b4b1
alebal123bal <balzanalessandro2001@gmail.com>
2024-06-04 20:06:35 +0200

    Same

--- include/MultivariablePolynomial.h
+++ include/MultivariablePolynomial.h
@@ -14,22 +14,42 @@ private:
 public:
     std::vector<Monomial> monomialVec;
 
+    //Polynomial class constructors
+
     MultivariablePolynomial();  // Default constructor initialized empty
     MultivariablePolynomial(const std::vector<Monomial>& monomials);  // Parametrized constructor
     
+    //Polynomial construction and definition functions
+
     void addMonomial(double coefficient, const std::vector<int>& exponents);
     void addMonomial(const Monomial& mono);
+
+    //Standard operators
+
     MultivariablePolynomial operator+(const MultivariablePolynomial& other) const;
     MultivariablePolynomial operator-(const MultivariablePolynomial& other) const;
     MultivariablePolynomial operator*(const MultivariablePolynomial& other) const;
     std::pair<MultivariablePolynomial, MultivariablePolynomial> operator/(const MultivariablePolynomial& other) const;
+
+    //Custom functions
+
     MultivariablePolynomial pow(int exponent) const;
+    std::vector<MultivariablePolynomial> factorize() const; //Implement Groebner's
+
+    //Equality check
 
     bool operator==(const MultivariablePolynomial& other);
 
+    //Housekeeping functions
+
     void cleanup();
+    void ordering(int lexicographic_ordering);
+
+    //Numerical evalutation functions
 
     double eval(const std::vector<double>& values) const;
+
+    //Display functions
 
     void print() const;
 
